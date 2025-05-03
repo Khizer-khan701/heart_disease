4e742c049a137821bb0cf029bf1d3d5d1d149693
Khizer-khan701 <khizerdawood987@gmail.com>
2025-05-04 01:13:36 +0500

    exception done

---
+++ src/logger.py
@@ -0,0 +1,23 @@
+import os
+import logging
+import datetime
+from src.exception import CustomException
+import sys
+
+LOG_FILE=f"{datetime.datetime.now().strftime("%d_%M_%Y_%H_%M_%S")}.log"
+file_path=os.path.join(os.getcwd(),"Logs")
+os.makedirs(file_path,exist_ok=True)
+LOG_FILE_PATH=os.path.join(file_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(name)s %(lineno)d - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+if __name__=="__main__":
+    try:
+        logging.info("execution started")
+    except Exception as e:
+        raise CustomException(e,sys)
+
